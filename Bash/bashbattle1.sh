9bde975a4e37e4726b9b2901924ca65b1f638f00
SMAN04 <shalman004@gmail.com>
2026-03-27 15:29:39 +0000

    new folder and file

---
+++ Bash/bashbattle1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+read -rp "enter first number: " a
+
+read -rp "enter second number: " b
+
+
+echo "Addition: $((a + b))"
+echo "Subtraction: $((a - b))"
+echo "Multiplication: $((a * b))"
+echo "Division: $((a / b))"
+
+
+
+
